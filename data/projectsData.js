ba3b498ebc588fcb1182f9026f48f62ad176d297
prjctimg <prjctimg@outlook.com>
2023-02-27 02:20:08 +0200

    Updated siteMetaData.js. [Must add site-logo, news letter and edit analytics provider]

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,20 +1,15 @@
 const projectsData = [
+
+  // This data is mapped to the cards on the projects page
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
+    title: 'Huetiful',
+    description: `A small-ish JavaScript library of theme and palette generating utilities.`,
+
+    /* Add the path to to my project cover graphic */
     imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
-  },
+    href: 'https://github.com/prjctimg/huetiful',
+  }
+
 ]
 
 export default projectsData
