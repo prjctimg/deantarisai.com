7b654ae44fc22be8a1cbc6284ef94e37deda5489
Dean Tarisai <prjctimg@outlook.com>
2023-03-02 17:14:49 +0200

    Update Footer.js
    Escaped some HTML sensitive symbols.

--- components/Footer.js
+++ components/Footer.js
@@ -22,7 +22,7 @@ export default function Footer() {
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
           <Link href="https://github.com/prjctimg/deantarisai.com">
-            </> with ♥ in Zimbabwe.
+            &gt/&lt with ♥ in Zimbabwe.
           </Link>
         </div>
       </div>
