4fcd3f1e0c037fa9058777d98b405c5c0325cee0
Dean Tarisai <prjctimg@outlook.com>
2023-03-02 18:48:16 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,7 +25,7 @@ module.exports = {
         14: '3.5rem',
       },
       fontFamily: {
-        sans: ['LatoVariable'],
+        sans: ['Lato'],
       },
       colors: {
         primary: colors.teal,
